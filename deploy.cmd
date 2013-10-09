43deebebcc8dc2c160fda2193d75d6ab0ac96bc8
Alxandr <alxandr@alxandr.me>
2013-10-09 18:37:58 +0200

    deploy

--- deploy.cmd
+++ deploy.cmd
@@ -107,8 +107,8 @@ call :SelectNodeVersion
 :: 3. Install npm packages
 IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
   pushd %DEPLOYMENT_TARGET%
-  echo Running npm install
-  call !NPM_CMD! install
+  echo Running npm install --production
+  call !NPM_CMD! install --production
   echo Running grunt build
   call !GRUNT_CMD! build
   IF !ERRORLEVEL! NEQ 0 goto error
